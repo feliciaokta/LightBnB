26ad382e57af52d74ea09e0cba015a499c43d775
felicia puspita yuwono <feliciaokta@outlook.com>
2021-05-27 15:55:05 -0700

    worked function added all properties database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -180,8 +180,9 @@ exports.getAllProperties = getAllProperties;
  const addProperty = (property) => {
   return pool
     .query(
-      `INSERT INTO properties VALUES ($1);`,
-      [property])
+      `INSERT INTO properties VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+      RETURNING *;`,
+      [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
       .then((result) => {
         console.log(result.rows);
       })
